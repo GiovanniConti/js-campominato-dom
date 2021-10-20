a68b9ca373f386bd47d6af61c147482377dd782f
GiovanniConti <giovanni.conti@live.it>
2021-10-20 17:53:55 +0200

    completed

--- js/script.js
+++ js/script.js
@@ -11,14 +11,17 @@ btn.addEventListener("click", function(){
     const cellsNumber = GetCellsNumber(difficulty)
     // console.log(cellsNumber);
 
-    GenerateGrid(cellsNumber);
-    // function to generate array bomb (generate an array with rundom number that indicates the number of the cells that contain a bomb)
+    // function that resets (removes) the overlay
+    RemoveOverlay()
+
+    GenerateGrid(cellsNumber, bombsNum);
+    // function to generate array bomb (generate an array with random numbers that indicates the number of the cells that contain a bomb)
     newBombArray = BombArray(bombsNum, cellsNumber);
-    console.log(newBombArray);
-    
+    // console.log(newBombArray); ==> to see where the bombs are positioned in the grid
+
 });
 
-// Function that will return the number of cells after the player choose the difficulty level
+// Function that will return the number of cells after the player chose the difficulty level
 function GetCellsNumber(difficulty){
     let result;
 
@@ -40,22 +43,28 @@ function GetCellsNumber(difficulty){
 }
 
 // function that generate the grid after getting the number of cells
-function GenerateGrid(cellsNumber){
+function GenerateGrid(cellsNumber, bombsNum){
     // to empty the element "grid" if we created some previous grids
     grid.innerHTML = "";
 
     const cellSize = 100 / Math.sqrt(cellsNumber);
 
     for(i=0; i < cellsNumber; i++){
-        //create the cell(div) and give the classes and the right dimentions to size the cell
+        //creates the cell(div) and gives the classes and the right dimentions to size the cell
         const cell = document.createElement("div");
+        
         cell.classList.add("box")
         cell.style.width = cellSize + "%";
         cell.style.height = cellSize + "%";
+        
         // add the number of the cell
         cell.cellNumber = i + 1;
         cell.textContent = cell.cellNumber;
         // console.log("consoleLog di cell", cell);
+        
+        // number of cells without bombs
+        cell.bombfreecells = cellsNumber - bombsNum;
+
         // append "cell" to the html
         grid.append(cell);
 
@@ -66,24 +75,31 @@ function GenerateGrid(cellsNumber){
 
 }
 
-// function to give the class "clicked" to a cell
+// function called once clicked on a cell
 function OnCellClick(){
 
     let currentCellNum = parseInt(this.cellNumber);
 
-    if(newBombArray.includes(currentCellNum)){
-        this.classList.add("bomb");
-
+    // create an array to get all the clicked cells
     let clickedArray = document.querySelectorAll(".clicked");
     // console.log("clicked array", clickedArray);
+
     let points = clickedArray.length;
-    console.log("Points:", points);
-        
+    // console.log("Points:", points);
+
+    if(newBombArray.includes(currentCellNum)){
+        this.classList.add("bomb");
+        DisplayOverlay(points, true)
     } else{
         this.classList.add("clicked");
     }
+
+    if(points == this.bombfreecells - 1){
+        DisplayOverlay(points, false);
+    }
 }
 
+// function to generate an array that contains the positions of the bombs
 function BombArray(bombs, maxRandomNum){
     let bombArray = [];
 
@@ -98,4 +114,42 @@ function BombArray(bombs, maxRandomNum){
         }
     }
     return bombArray;
+}
+
+// function to display the overlay once clicked on a bomb or clicked all the bomb free cells
+function DisplayOverlay(points, clickedBomb){
+
+    // adds class displayOverlay to resultOverlay
+    const displayOverlay = document.getElementById("resultsOverlay");
+    displayOverlay.classList.add("displayOverlay");
+
+    // checks if clicked a bomb or if clicked all bombs free cells and displays the correct overlay
+    if(clickedBomb){
+        const gameOver = document.getElementById("gameOver");
+        gameOver.classList.add("displayed");
+    } else{
+        const win = document.getElementById("win");
+        win.classList.add("displayed");
+        points = points+1;
+    }
+
+    // adds to the overlay the ammount of points gained
+    const score = document.getElementById("score");
+    score.innerHTML  = points;
+}
+
+// function to reset (remove) the overlay and play again
+function RemoveOverlay(){
+    const displayOverlay = document.getElementById("resultsOverlay");
+    displayOverlay.classList.remove("displayOverlay");
+
+    const gameOver = document.getElementById("gameOver");
+    gameOver.classList.remove("displayed");
+
+    const win = document.getElementById("win");
+    win.classList.add("displayed");
+
+    // removees the ammount of points gained the previous game
+    const score = document.getElementById("score");
+    score.innerHTML  = "";
 }
